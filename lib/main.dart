581fc55ff7041ecd2a825872e5effad0d961d740
dz1 <MaHxop@uandex.ru>
2022-04-17 16:15:05 +0300

    Домашнее задание 1 v.0.3.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:dz1/film.dart';
 
 void main() {
-  FilmData film = FilmData(language: 'Французский');
-  print(film.stringToLang(film.language ?? ''));
   List<FilmData> filmList = FilmData.testFilm;
   ratingFilm(filmList);
 }
